68bde50b152637befea44c98047ab51a2bc4469b
bustomii <tomblok.id@gmail.com>
2024-09-05 23:51:39 +0700

    setup email smtp

---
+++ laravel/app/Mail/Supervisor/MailSupervisor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail\Supervisor;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MailSupervisor extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        $data = $this->data;
+        return $this->subject($data['TITLE'])->view('mails.supervisor', $data);
+    }
+}
---
+++ laravel/resources/views/mails/supervisor.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Superviso</title>
+</head>
+
+<body>
+    <h2>Hi, Customer Service</h2>
+    <p>Nasabah dengan nama {{ $data['nama'] }} telah {{ $data['status'] }} dengan nominal {{ $data['nominal'] }}.</p>
+    <p>Terima kasih.</p>
+</body>
+
+</html>
